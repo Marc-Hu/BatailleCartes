0d04496a8ab62e4f9c76b1e47e9767a4b2de150a
Marc <marc.hu@outlook.fr>
2017-03-23 11:26:40 +0100

    Ajout des méthodes de la classe Main (ajouterCarte, retirerCarte et mainVide)

--- src/Bataille/Main.java
+++ src/Bataille/Main.java
@@ -20,4 +20,30 @@ public class Main {
 	public Deque<Carte> getMain(){
 		return this.cartesMain;
 	}
+	
+	/**
+	 * Méthode de la classe Main qui va ajouter une carte à la fin du Deque
+	 * @param carteAjoute Carte à ajouter dans le Deque
+	 */
+	
+	public void ajouterCarte(Carte carteAjoute){
+		this.cartesMain.offerLast(carteAjoute);
+	}
+	
+	/**
+	 * Méthode de la classe Main qui va retirer la carte qui se situe tout en haut du Deque
+	 */
+	
+	public void retirerCarte(){
+		this.cartesMain.pop();
+	}
+	
+	/**
+	 * Méthode de la classe Main qui va vérifier qui la Main est vide.
+	 * @return True si le Deque est vide et False si le Deque n'est pas vide.
+	 */
+	
+	public boolean estVide(){
+		return this.cartesMain.isEmpty();
+	}
 }
