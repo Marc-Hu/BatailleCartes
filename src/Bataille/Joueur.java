31b8d77fddf5c0ab825d1d1283b986768df45736
Marc <marc.hu@outlook.fr>
2017-03-23 10:30:50 +0100

    Ajout des classes Joueur et Partie

---
+++ src/Bataille/Joueur.java
@@ -0,0 +1,11 @@
+package Bataille;
+
+public class Joueur {
+	private String nom;
+	private int nbPartiesGagnees;
+	
+	public Joueur(String nomJoueur, int nbPartieGagne){
+		this.nom= nomJoueur;
+		this.nbPartiesGagnees=nbPartieGagne;
+	}
+}
