90d6e30d5882a253b22583bffa2f5621f5dcbc60
Marc <marc.hu@outlook.fr>
2017-03-23 11:08:36 +0100

    Ajout du constructeur et du getter de la classe Main

--- src/Bataille/Main.java
+++ src/Bataille/Main.java
@@ -1,5 +1,23 @@
 package Bataille;
+import java.util.*;
 
 public class Main {
-
+	private Deque<Carte> cartesMain;
+	
+	/**
+	 * Constructeur de la classe Main.
+	 */
+	
+	public Main(){
+		this.cartesMain=new ArrayDeque<Carte>();
+	}
+	
+	/** 
+	 * Méthode get de la classe Main qui va renvoyer les cartes dans la main
+	 * @return Les cartes en main
+	 */
+	
+	public Deque<Carte> getMain(){
+		return this.cartesMain;
+	}
 }
