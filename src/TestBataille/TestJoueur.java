af3951e7ca1afd5673e2c0f680ff0687e0744829
jerent Steeve <jerent.steeve@gmail.com>
2017-03-24 15:25:35 +0100

    Mise a jour des tests sur joueur

--- src/TestBataille/TestJoueur.java
+++ src/TestBataille/TestJoueur.java
@@ -4,8 +4,19 @@ import Bataille.*;
 public class TestJoueur {
 	public static void main(String[] args){
 		System.out.println("Test du constructeur de la classe Joueur et test de la méthode ditBataille");
-		Joueur marc = new Joueur("Marc");
+		Main mainDeMarc = new Main();
+		mainDeMarc.ajouter(new Carte (8, "pique"));
+		mainDeMarc.ajouter(new Carte (9, "pique"));
+		mainDeMarc.ajouter(new Carte (6, "pique"));
+		Pile pileDeMarc = new Pile();
+		Joueur marc = new Joueur("Marc", mainDeMarc, pileDeMarc);
 		System.out.println(marc);
 		marc.ditBataille();
+		System.out.println("Test de la méthode poserUneCarte");
+		marc.poserUneCarte();
+		if (marc.getNbCartesEnMain()==2 && marc.getNbCartesPile()==1)
+			System.out.println("Marc à bien 2 cartes en main et une carte sur sa pile.");
+		else
+			System.out
 	}
 }
