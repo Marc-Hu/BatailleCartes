0901c4465fa3998be69cc9c0cd5d8962cd9bcba0
jerent Steeve <jerent.steeve@gmail.com>
2017-03-24 16:02:29 +0100

    Test pour la classe Pile terminé

--- src/TestBataille/TestPile.java
+++ src/TestBataille/TestPile.java
@@ -4,20 +4,32 @@ import java.util.*;
 
 public class TestPile {
 	public static void main(String[] args){
-		System.out.println("Test du constructeur de la classe Pile et de la méthode d'ajout de carte dans la pile");
+		System.out.println("=====>Test du constructeur de la classe Pile et de la méthode d'ajout de carte dans la pile");
 		Pile pileDeMarc = new Pile();
 		pileDeMarc.ajouter(new Carte (10, "trefle"));
 		System.out.println(pileDeMarc);
-		System.out.println("test de la méthode estVide");
-		System.out.println(pileDeMarc.estVide());
-		Stack<Carte> test = pileDeMarc.viderPile();
-		System.out.println(test);
-		System.out.println(pileDeMarc.estVide());
-		System.out.println("Test de la méthode taille");
+		if (pileDeMarc.taille()==1)
+			System.out.println("Test réussi! La pile de Marc contient bien une carte");
+		else
+			System.out.println("Erreur! La pile de Marc contient une carte et non "+pileDeMarc.taille()+" cartes.");
+		System.out.println("=====>Test de la méthode estVide");
+		if (!pileDeMarc.estVide())
+			System.out.println("Test réussi! La pile de Marc contient des cartes");
+		else
+			System.out.println("Erreur! La pile de Marc n'est pas vide.");
+		pileDeMarc.viderPile();
+		if (pileDeMarc.estVide())
+			System.out.println("Test réussi! La pile de Marc est vide.");
+		else
+			System.out.println("Erreur! La piple de Marc est vide!");
+		System.out.println("=====>Test de la méthode taille");
 		pileDeMarc.ajouter(new Carte (10, "trefle"));
 		pileDeMarc.ajouter(new Carte (9, "trefle"));
 		pileDeMarc.ajouter(new Carte (8, "trefle"));
 		System.out.println(pileDeMarc);
-		System.out.println("Taille de la pile : "+pileDeMarc.taille());
+		if (pileDeMarc.taille()==3)
+			System.out.println("Test réussi! La pile de Marc contient bien 3 cartes");
+		else
+			System.out.println("Erreur! La pile de Marc contient 3 cartes et non "+pileDeMarc.taille()+ " cartes.");
 	}
 }
