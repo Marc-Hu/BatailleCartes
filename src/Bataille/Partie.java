03cc8cad7668db2dd45dcc16c4d022d93fd17261
BSpirit <clonier.tony@hotmail.fr>
2017-03-24 17:19:33 +0100

    Modifs Joueur + Main

--- src/Bataille/Partie.java
+++ src/Bataille/Partie.java
@@ -1,18 +1,37 @@
 package Bataille;
 import java.util.HashSet;
 
+/**
+ * Classe de gestion d'une partie
+ * @version 1.0
+ * @author Tony Clonier
+ */
 public class Partie {
 	
 	private HashSet<Joueur> joueurs;
-	private Paquet paquetJ;
+	private Paquet paquet;
 	
+/*Constructeur*/
 	/**
-	 * Permet d'initialiser une Partie.
-	 * @param initJoueur est un tableau de joueur
+	 * Créer une instance de partie 
+	 * @param joueurs est un tableau de joueur
+	 * @param paquet
 	 */
-	public Partie(HashSet<Joueur> initJoueur, Paquet paquetJoueur){
-		this.joueurs = initJoueur;
-		this.paquetJ = paquetJoueur;
+	public Partie(HashSet<Joueur> joueurs, Paquet paquet){
+		this.joueurs = joueurs;
+		this.paquet = paquet;
+	}
+	
+/*Méthodes*/
+	
+	public String toString(){
+		String str = new String();
+		str += "Partie en cours : \n";
+		str += "Liste des joueurs : \n";
+		for(Joueur j : this.joueurs)
+			str += j.toString()+'\n';
+		
+		return str;
 	}
 	
 //	public boolean estTerminer(){
